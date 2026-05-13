980b61361526e5512d7941e8577331853993c0ec
seo youndeok <luckyguyseo@gmail.com>
2026-05-13 22:43:21 +0900

    Fix missing Response import

--- api/main.py
+++ api/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, Response
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
 from contextlib import asynccontextmanager
